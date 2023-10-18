5df04ef519c590340582166124592efaa8345670
zNahuelz <102327040+zNahuelz@users.noreply.github.com>
2023-10-18 15:36:14 -0500

    Update RestApiApplication.java

--- rest_api/src/main/java/com/longdrink/rest_api/RestApiApplication.java
+++ rest_api/src/main/java/com/longdrink/rest_api/RestApiApplication.java
@@ -24,7 +24,7 @@ public class RestApiApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
 			}
 		};
 	}

fbf203ce3d157722bcd2cf20eb1e2ff560f2e84f
zNahuelz <102327040+zNahuelz@users.noreply.github.com>
2023-10-18 14:58:56 -0500

    Fix: CORS
    - Habilitada la recepción de solicitudes desde el frontend web.

--- rest_api/src/main/java/com/longdrink/rest_api/RestApiApplication.java
+++ rest_api/src/main/java/com/longdrink/rest_api/RestApiApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestApiApplication {
@@ -15,5 +17,15 @@ public class RestApiApplication {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder(){
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/").allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 }

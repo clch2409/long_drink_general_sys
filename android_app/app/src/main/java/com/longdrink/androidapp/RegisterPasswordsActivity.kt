bb5f04c6841b607cd4fbc761ca9786d9d78a734d
clch2409 <cesarlch2409@gmail.com>
2023-10-20 15:05:28 -0500

    Creación de vistas
    Programación de vistas para login y registro. Organización de strings y dimens

--- android_app/app/src/main/java/com/longdrink/androidapp/RegisterPasswordsActivity.kt
+++ android_app/app/src/main/java/com/longdrink/androidapp/RegisterPasswordsActivity.kt
@@ -3,9 +3,9 @@ package com.longdrink.androidapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RegisterPasswordsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register_passwords)
     }
 }

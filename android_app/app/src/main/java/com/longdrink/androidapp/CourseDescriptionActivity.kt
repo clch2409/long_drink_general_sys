c37a916830e13b437f8ecfd8126b0342ab92c203
clch2409 <cesarlch2409@gmail.com>
2023-10-23 11:07:52 -0500

    Vistas Terminadas
    Estructuras de vistas terminadas. Pasando a programación y consumo de API

---
+++ android_app/app/src/main/java/com/longdrink/androidapp/CourseDescriptionActivity.kt
@@ -0,0 +1,11 @@
+package com.longdrink.androidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CourseDescriptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_description)
+    }
+}

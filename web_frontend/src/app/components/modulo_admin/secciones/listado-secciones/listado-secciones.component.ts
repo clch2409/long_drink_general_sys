011c3c827738a6da74318fba939439b06eada417
elmerctl <elmerctl12@gmail.com>
2023-12-16 12:11:43 -0500

    secciones

--- web_frontend/src/app/components/modulo_admin/secciones/listado-secciones/listado-secciones.component.ts
+++ web_frontend/src/app/components/modulo_admin/secciones/listado-secciones/listado-secciones.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Seccion } from 'src/app/models/seccion.model';
+import { SeccionService } from 'src/app/services/seccion.service';
+import { StorageService } from 'src/app/services/storage.service';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-listado-secciones',
   templateUrl: './listado-secciones.component.html',
   styleUrls: ['./listado-secciones.component.css']
 })
-export class ListadoSeccionesComponent {
+
+
+export class ListadoSeccionesComponent{
+
+  secciones: Seccion[] = [];
+
+  constructor( private storageService: StorageService, private router: Router,private seccionService: SeccionService) { }
+
+  getSecciones(): void{
+    this.seccionService.getSecciones().subscribe({
+      next: (data) =>{
+        this.secciones = data;
+        console.log(data);
+      },
+      error: (err) => console.log(err)
+    });
+  }
+
 
 }
+
+ 
+
+
+
+
